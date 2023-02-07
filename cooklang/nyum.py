c28aeb9fd8da3d0c463e5b6230620dddf4747d32
Alex Guinman <alex@guinman.id.au>
2023-02-07 21:04:23 +1000

    Convert nyum the other way

--- cooklang/nyum.py
+++ cooklang/nyum.py
@@ -4,6 +4,9 @@ https://github.com/doersino/nyum
 """
 
 from pathlib import Path
+from shutil import copy
+
+import frontmatter
 
 from cooklang import Recipe
 
@@ -53,3 +56,60 @@ def to_nyum_markdown(file_path: Path) -> list[str]:
     del output[-1]  # remove last line break
 
     return output
+
+
+def from_nyum_markdown(file_path: Path) -> list[str]:
+    with open(file_path) as fh:
+        metadata, content = frontmatter.parse(fh.read())
+
+    output = []
+    desc = metadata.get("description", "")
+    if desc:
+        output.append(f">> description: {desc}")
+    output.append("")
+    steps = content.split("---")
+    for step in steps:
+        step_output = ""
+        step = step.replace("> ", "")
+        lines = step.split("\n")
+        for line in lines:
+            if line and line[0] == "*":
+                if "`" in line:
+                    x = line.split("`")
+                    quantity = x[1].strip()
+                    if len(x) > 2:
+                        ingredient = x[2].strip()
+                        cook_ingr = "@" + ingredient + "{" + quantity + "}"
+                    else:
+                        ingredient = quantity
+                        cook_ingr = "@" + ingredient + "{}"
+                else:
+                    ingredient = line.replace("* ", "").strip()
+                    cook_ingr = "@" + ingredient + "{}"
+                step_output += cook_ingr + " "
+            else:
+                if line:
+                    step_output += line + " "
+        output.append(step_output)
+        output.append("")
+
+    return metadata, output
+
+
+def migrate_nyum_to_cook(nyum_dir: Path, output_dir: Path):
+    """Migrate a directory of nyum recipes to cook files"""
+    for file_path in nyum_dir.glob("*.md"):
+        meta, output = from_nyum_markdown(file_path)
+        title = meta["title"]
+        category = meta["category"]
+        category_dir = output_dir / category
+        category_dir.mkdir(exist_ok=True, parents=True)
+        cook_path = category_dir / f"{title}.cook"
+
+        img_path = file_path.with_suffix(".jpg")
+        if img_path.exists():
+            cook_img_path = category_dir / f"{title}.jpg"
+            copy(img_path, cook_img_path)
+
+        with open(cook_path, "w") as f:
+            f.writelines([x + "\n" for x in output])
