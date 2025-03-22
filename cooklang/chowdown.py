c61a383abfd9518cb9e7c39af1f73267ec14d4f6
Alex Guinman <alex@guinman.id.au>
2025-03-23 09:44:29 +1000

    Add tags to chowdown

--- cooklang/chowdown.py
+++ cooklang/chowdown.py
@@ -11,7 +11,9 @@ from slugify import slugify
 from . import Recipe
 
 
-def to_chowdown_markdown(file_path: Path, img_path: str = "") -> list[str]:
+def to_chowdown_markdown(
+    file_path: Path, img_path: str = "", category: str = ""
+) -> list[str]:
     r = Recipe(file_path)
     ast = r.ast
     title = r.title
@@ -28,6 +30,13 @@ def to_chowdown_markdown(file_path: Path, img_path: str = "") -> list[str]:
         output.append(f"image: {img_path}")
     for key, value in metadata.items():
         output.append(f"{key}: {value}")
+
+    tags = [category] if category else []
+    output.append(" ")
+    output.append("tags:")
+    for tag in tags:
+        line = f'- "{tag}"'
+        output.append(line)
 
     output.append(" ")
     output.append("ingredients:")
@@ -93,6 +102,7 @@ def migrate_cook_to_chowdown(cook_dir: Path, output_dir: Path) -> int:
     for file_path in cook_files:
         title = file_path.stem
         slug_title = slugify(title)
+        category = file_path.parent.name if file_path.parent != cook_dir else ""
 
         img_path = file_path.with_suffix(".jpg")
         if img_path.exists():
@@ -102,7 +112,7 @@ def migrate_cook_to_chowdown(cook_dir: Path, output_dir: Path) -> int:
         else:
             img_path = ""
 
-        output = to_chowdown_markdown(file_path, img_path=img_path)
+        output = to_chowdown_markdown(file_path, img_path=img_path, category=category)
 
         chow_path = output_dir_recipes / f"{slug_title}.md"
         with open(chow_path, "w") as f:
